960ef8312dbedecd5291c8d0ed1ebfe5f00adc78
maxel <axel.maerten@ynov.com>
2023-05-26 10:19:44 +0200

    syntax correction

--- src/Pneus/PneuEte.java
+++ src/Pneus/PneuEte.java
@@ -4,9 +4,11 @@ public class PneuEte extends Pneu{
 
     public PneuEte(String marque, int largeurPneu, int indiceCapaciteCharge, String dateProduction, int indiceAdherence) {
         super(marque, largeurPneu, indiceCapaciteCharge, dateProduction, indiceAdherence);
-        if (indiceAdherence < 0.4 || indiceAdherence > 0.7);
-        throw new IllegalArgumentException("L'indice d'aderation des pneus toute saison ont une adhérence 0.4 et 0.7");
-
+        if (indiceAdherence < 0.4 || indiceAdherence > 0.7) {
+            throw new IllegalArgumentException("L'adhérence des pneus Toute Saison doit être située entre 0.4 et 0.7.");
+        }
     }
+
+
 
 }
